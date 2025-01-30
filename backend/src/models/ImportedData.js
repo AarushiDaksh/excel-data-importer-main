a636851d6449b00e275cb9a2edcaa257238aaedf
Aarushi Daksh <136158651+AarushiDaksh@users.noreply.github.com>
2025-01-30 11:02:26 +0530

    hie
    hie

---
+++ backend/src/models/ImportedData.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const ImportedDataSchema = new mongoose.Schema({
+  sheetName: String,
+  Name: String,
+  Amount: Number,
+  Date: Date,
+  Verified: String,
+});
+
+const ImportedData = mongoose.model("ImportedData", ImportedDataSchema);
+export default ImportedData;
